04ab0e357e2e2abba5269debd5e1b469c95ef18a
hannael_abrams <hannaelabrams@gmail.com>
2024-07-25 12:43:56 +0200

    clear file and setup the project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,16 @@
 module.exports = {
   root: true,
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json', './tsconfig.app.json'],
+    tsconfigRootDir: __dirname,
+  },
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
