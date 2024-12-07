55736d2a128e6eb271352a14cd5207febd363fa2
hannael_abrams <hannaelabrams@gmail.com>
2024-12-07 01:06:05 +0100

    remove default url

--- src/core/constants/index.ts
+++ src/core/constants/index.ts
@@ -1,5 +1,4 @@
 export const HOUSEHOLD_API_HOST =
   import.meta.env.HOUSEHOLD_API || 'http://localhost:4000';
 
-export const AUTH_SERVICE_API_HOST =
-  import.meta.env.AUTH_SERVICE_API || 'http://localhost:4444';
+export const AUTH_SERVICE_API_HOST = import.meta.env.AUTH_SERVICE_API;
