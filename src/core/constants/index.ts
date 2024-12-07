85bdf04498ef8f618d79ae20c135462b83687f28
hannael_abrams <hannaelabrams@gmail.com>
2024-12-07 01:09:12 +0100

    add url backend

--- src/core/constants/index.ts
+++ src/core/constants/index.ts
@@ -1,4 +1,6 @@
 export const HOUSEHOLD_API_HOST =
   import.meta.env.HOUSEHOLD_API || 'http://localhost:4000';
 
-export const AUTH_SERVICE_API_HOST = import.meta.env.AUTH_SERVICE_API;
+export const AUTH_SERVICE_API_HOST =
+  import.meta.env.AUTH_SERVICE_API ||
+  'https://auth-service-1-eu3e.onrender.com';
